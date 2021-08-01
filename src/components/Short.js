0fb32158b7d49d09568b8ce90379a3ca0bc06202
Henry <53154228+sickerman@users.noreply.github.com>
2021-08-01 10:59:12 -0400

    implemented successful api call

--- src/components/Short.js
+++ src/components/Short.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState} from 'react'
 
 function Short() {
+    const [link, setLink] = useState("");
+    const [short, setShort]= useState("");
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        getLink();
+    }
+
+    const getLink = async ()=> {
+        await axios
+        .get(`https://api.shrtco.de/v2/shorten?url=${link}`)
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+
+    }
     return (
         <section id="form" className="container short">
-            <form action="">
-                <input type="text" placeholder="shorten a link here..."/>
-                <a className="lg-rect-btn" href="/">Shorten it!</a>
+            <form onSubmit={(e)=> handleSubmit(e)} >
+                <input 
+                type="text" 
+                value={link} 
+                onChange={(e)=> setLink(e.target.value)}
+                placeholder="shorten a link here..."/>
+                <a onClick={(e)=> handleSubmit(e)} className="lg-rect-btn" href="/">Shorten it!</a>
             </form>
         </section>
     )
